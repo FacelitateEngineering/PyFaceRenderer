648d32aeb7d8e7009bb4128330fc70bd6e03f815
Toby <tobyclh@gmail.com>
2023-06-22 20:44:37 +0900

    enable logging

--- example.py
+++ example.py
@@ -1,8 +1,11 @@
 import dearpygui.dearpygui as dpg
 from PyFaceRenderer import FaceRenderer
+import logging as log
+log.basicConfig(level='DEBUG')
 dpg.create_context()
 dpg.configure_app(docking=True, docking_space=True, )
 dpg.create_viewport(title=f'PyFaceRenderer Sample', width=1920, height=1080, always_on_top=True, )
+# fr = FaceRenderer('mediapipe', wireframe=False)
 fr = FaceRenderer('fuze', wireframe=False)
 fr.show_face_renderer(show_control=True)
 
