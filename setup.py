fe357877951873a2c5c7e773adbb62140c1f79ed
toby <tobyclh@gmail.com>
2023-06-26 10:12:18 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages, distutils
+
+setup(name='PyFaceRenderer',
+      version='1.0',
+      description='A face renderer for test',
+      author='Toby Chong',
+      author_email='tobyclh@gmail.com',
+      license='Copyright',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=False)
