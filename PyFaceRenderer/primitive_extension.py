8fc51e557246ec80f25998ca58ad06caed43de77
toby <tobyclh@gmail.com>
2023-06-30 21:20:33 +0900

    add primitive

--- PyFaceRenderer/primitive_extension.py
+++ PyFaceRenderer/primitive_extension.py
@@ -8,10 +8,11 @@ def upload_vertex_data(primitive):
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer)
 
     if hasattr(primitive, 'face_renderer_buffer'):
-        primitive.face_renderer_buffer[:, :3] = primitive.positions
+        primitive.face_renderer_buffer[primitive.face_renderer_indice] = primitive.positions.reshape(-1).astype(np.float32)
     else:
         # positions
         vertex_data = primitive.positions
+        n_vertex = vertex_data.shape[0]
         attr_sizes = [3]
 
         # Normals
@@ -36,7 +37,22 @@ def upload_vertex_data(primitive):
         if primitive.color_0 is not None:
             vertex_data = np.hstack((vertex_data, primitive.color_0))
             attr_sizes.append(4)
+        vertex_data = np.ascontiguousarray(
+            vertex_data.flatten().astype(np.float32)
+        )
+
         primitive.face_renderer_buffer = vertex_data
+        total_attr_size = sum(attr_sizes)
+        # index_buffer = np.zeros(total_attr_size, dtype=int)
+        _idx = np.arange(n_vertex) * total_attr_size
+        indice_array = np.concatenate([_idx, _idx+1, _idx+2])
+        indice_array.sort()
+        print(f'indice_array: {indice_array.shape} {indice_array[:10]}')
+        primitive.face_renderer_indice = indice_array
+
+
+
+
     
     vertex_data = np.ascontiguousarray(
         primitive.face_renderer_buffer.flatten().astype(np.float32)
